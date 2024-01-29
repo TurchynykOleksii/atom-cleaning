3f8e4649027c9426439b29b7d37fa4b5a7f0fa34
Turchynyk Oleksii <mishkalesha1994@gmail.com>
2024-01-29 21:55:50 +0200

    init atom cleaning project

---
+++ dev/js/modules/slider.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper(".clean__inner .swiper", {
+  slidesPerView: "1",
+  loop: true,
+
+  navigation: {
+    nextEl: ".clean__inner .swiper-button-next",
+    prevEl: ".clean__inner .swiper-button-prev",
+  },
+  spaceBetween: 20,
+  autoHeight: true,
+});
+
+const swiperReview = new Swiper(".swiper.reviews__swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  roundLengths: true,
+  initialSlide: 1,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 120,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    clickable: true,
+    nextEl: ".reviews__inner .swiper-button-next",
+    prevEl: ".reviews__inner .swiper-button-prev",
+  },
+});
